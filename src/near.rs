d5674e6a632104a6d7aec24e8980b382004db641
The SunShining <thesunshiningmin@gmail.com>
2025-03-21 16:17:07 -0600

    re

--- src/near.rs
+++ src/near.rs
@@ -1,28 +1,45 @@
-use near_api::NearApi;
-use serde_json::json;
+use near_sdk::serde_json::json;
+use near_sdk::AccountId;
+use reqwest;
 use crate::config::NetworkConfig;
 
+#[derive(Debug)]
 pub struct NearClient {
     config: NetworkConfig,
-    api: NearApi,
+    client: reqwest::Client,
 }
 
 impl NearClient {
     pub fn new() -> Self {
         let config = NetworkConfig::default();
-        let api = NearApi::new(&config.node_url);
-        Self { config, api }
+        let client = reqwest::Client::new();
+        Self { config, client }
     }
 
     pub async fn get_greeting(&self) -> Result<String, Box<dyn std::error::Error>> {
-        let args = json!({});
-        let result = self.api.view_function(
-            &self.config.contract_id,
-            "get_greeting",
-            args
-        ).await?;
+        let contract_id: AccountId = self.config.contract_id.parse()?;
+        let args_base64 = base64::encode(json!({}).to_string());
+
+        let query = json!({
+            "request_type": "call_function",
+            "finality": "final",
+            "account_id": contract_id.to_string(),
+            "method_name": "get_greeting",
+            "args_base64": args_base64
+        });
+
+        let response = self.client
+            .post(&self.config.node_url)
+            .json(&query)
+            .send()
+            .await?
+            .json::<serde_json::Value>()
+            .await?;
+
+        let result = response["result"]["result"]
+            .as_array()
+            .ok_or("Invalid response format")?;
         
-        let greeting: String = serde_json::from_value(result)?;
+        let greeting = String::from_utf8(base64::decode(result)?)?;
         Ok(greeting)
-    }
 }
